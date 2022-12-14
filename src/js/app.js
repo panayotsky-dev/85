052aacb30848a757b6d3c377bb8d3c287c6bd5e5
panayotsky.dev <panayotsky.dev@gmail.com>
2022-12-14 05:24:01 +0200

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,18 @@
-import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
-
-  const ul = document.querySelector("ul");
+  const pokemonCount = 10;
+  const ul = document.querySelector('ul');
+  fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${pokemonCount}`) 
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data);
+      data.results.forEach((pokemon) => {
+        let li = document.createElement('li');
+        li.innerText = pokemon.name
+        ul.appendChild(li)
+      });
+    });
+  
 });
+
+
